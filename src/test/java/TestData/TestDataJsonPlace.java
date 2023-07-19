b61055a09a818dd2433c508a8b7ab186180f749e
hayyam2395 <ogun.erdogan23@gmail.com>
2023-07-19 19:58:30 +0200

    19072023

---
+++ src/test/java/TestData/TestDataJsonPlace.java
@@ -0,0 +1,32 @@
+package TestData;
+
+import org.json.JSONObject;
+
+public class TestDataJsonPlace {
+
+    public int basariliStatusKodu = 200;
+    public String contentType = "application/json; charset=utf-8";
+    public String connectionHeaderDegeri = "keep-alive";
+
+
+   public JSONObject expectedBodyOlusturJSON(){
+
+       JSONObject expData = new JSONObject();
+       expData.put("userId",3);
+       expData.put("id",22);
+       expData.put("title","dolor sint quo a velit explicabo quia nam");
+       expData.put("body","eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse");
+       return expData;
+
+   }
+
+   public JSONObject requestBodyOlusturJSON(){
+
+       JSONObject reqBody = new JSONObject();
+       reqBody.put("title","Ali");
+       reqBody.put("body","Merhaba");
+       reqBody.put("userId",10);
+       reqBody.put("id",70);
+       return reqBody;
+   }
+}
---
+++ src/test/java/test/C21_Post_TestDataKullanimi.java
@@ -0,0 +1,6 @@
+package test;
+
+public class C21_Post_TestDataKullanimi {
+
+
+}
